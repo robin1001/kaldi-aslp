54715c0432281abad3350126daca6e488adf3397
hechangqing <494268393@qq.com>
2016-12-01 12:26:43 +0800

    edit run_energy_vad.sh

--- aslp_scripts/vad/run_energy_vad.sh
+++ aslp_scripts/vad/run_energy_vad.sh
@@ -4,6 +4,9 @@
 
 stage=1
 
+# test raw wav scp
+wav_test="scp:data/test/wav.scp"
+
 dir=exp/energy_vad
 
 echo "$0 $@"  # Print the command line for logging
@@ -15,6 +18,7 @@ set -euo pipefail
 
 sil_id=1
 alidir=exp/tri2b_ali
+
 labels_test="ark:ali-to-phones --per-frame=true $alidir/final.mdl \\\"ark:gunzip -c $alidir/ali.*.gz |\\\" ark:- | aslp-ali-to-sil --sil-id=${sil_id} ark:- ark:-|"
 
 # Test
